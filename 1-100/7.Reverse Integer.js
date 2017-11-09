5213483f3b623d051b1c37133716c6625743c9d4
Ali1213 <yusili2000@163.com>
2017-11-09 21:13:26 +0800

    fixed: resolve question 7

--- 1-100/7.Reverse Integer.js
+++ 1-100/7.Reverse Integer.js
@@ -31,3 +31,54 @@ var method1 = function(x) {
 
     return x > 2147483647 || x < -2147483648 ? 0 : x ;
 };
+
+var method2 = function(x) {
+
+    let isNegative,answer=0;
+
+    if(x < 0){
+        isNegative = true;
+        x = 0 - x;
+    }
+
+    let arr = [];
+
+    while( x > 0 ){
+        let b = Math.floor(x/10);
+        arr.push( x - b *10 )
+        x = b;
+    }
+
+    for(let i=0,len = arr.length;i<len;i++){
+        answer += Math.pow(10,len-i-1)*arr[i];
+    }
+
+    answer = isNegative ? 0 - answer : answer;
+
+    return answer > 2147483647 || answer < -2147483648 ? 0 : answer ;
+
+};
+
+
+//还是递归的思想好啊
+var method = function(x) {
+
+    let isNegative = 1,answer=0;
+
+    if(x < 0){
+        isNegative = -1;
+        x = 0 - x;
+    }
+
+    while(x>0){
+        answer = answer * 10 + x%10;
+        x = Math.floor(x/10);
+    }
+
+
+
+
+
+    return answer > 2147483647 ? 0 : answer*isNegative ;
+
+};
