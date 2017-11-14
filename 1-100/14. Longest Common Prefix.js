afc1ec793e2e6c4c543e1ba1e04aa946502b9843
Ali1213 <yusili2000@163.com>
2017-11-14 10:50:06 +0800

    add: question solution 2

--- 1-100/14. Longest Common Prefix.js
+++ 1-100/14. Longest Common Prefix.js
@@ -30,4 +30,44 @@ var longestCommonPrefix = function(strs) {
     return result;
 };
 
+/*
+Write a function to find the longest common prefix string amongst an array of strings.*/
+
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+
+    if(strs.length === 0){
+        return '';
+    }
+
+    if(strs.length === 1){
+        return strs[0];
+    }
+
+    let compare = strs[0],pos = 0;
+    for(let i = 1,len=strs.length;i<len;i++){
+        if(strs[i].length < compare.length){
+            compare = strs[i];
+            pos = i;
+        }
+    }
+    [strs[pos],strs[0]] = [strs[0], strs[pos]];
+
+    let results = compare.split(''),
+        result = '';
+
+    for(let i = 0,len=results.length;i<len;i++){
+        result += results[i];
+        for(let j = 1,jlen = strs.length;j<jlen;j++ ){
+            if(! strs[j].startsWith(result)){
+                return result.slice(0,-1);
+            }
+        }
+    }
+    return result;
+};
 
