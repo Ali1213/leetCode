69eeb3995f3bffd2b7862f1a6ebdff079b02ac53
Ali1213 <yusili2000@163.com>
2017-12-25 12:07:55 +0800

    before merge

--- 1-100/35. Search Insert Position.js
+++ 1-100/35. Search Insert Position.js
@@ -1,1 +1,37 @@
 // Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// beat 80.99%
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  if(target < nums[0]){
+      return 0;
+  }
+  if(target > nums[nums.length-1]){
+      return nums.length;
+  }
+  let pos = Math.floor(nums.length/2),
+      min = 0,
+      max = nums.length;
+  
+  while(max-min>1){
+      
+      if(nums[pos] === target){
+          return pos;
+      }
+      
+      if(nums[pos] > target){
+          max = pos;
+          pos = Math.floor((min+max)/2);
+      }
+      
+      if(nums[pos] < target){
+          min = pos;
+          pos = Math.floor((min+max)/2);
+      }
+  }
+  return nums[pos] >= target ? pos:pos+1;
+};
