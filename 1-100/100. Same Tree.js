55e5e506d3ff12875e39bfa9cb7ec09ab78a4aeb
Ali1213 <yusili2000@163.com>
2018-02-09 11:11:23 +0800

    add: resolve 100

--- 1-100/100. Same Tree.js
+++ 1-100/100. Same Tree.js
@@ -68,3 +68,33 @@ var isSameTree = function(p, q) {
   if(p.val !== q.val) return false;
   return true;
 };
+
+// 66.82% 要哭了。。。。。
+var isSameTree = function(p, q) {
+    
+  if(p===null || q ===null){
+      return p === q;
+  }
+  
+  if( !(isSameTree(p.left,q.left) && isSameTree(p.right,q.right)) ){
+      return false;
+  }
+  
+  return p.val === q.val;
+};
+
+// 1.82% 
+// 46.82%  两次提交显示的结果不一样。。。。。。
+// 说明这个时间上的测试真的是很随意
+var isSameTree = function(p, q) {
+    
+  if(p===null || q ===null){
+      return p === q;
+  }
+  
+  if( isSameTree(p.left,q.left) && isSameTree(p.right,q.right) ){
+      return p.val === q.val;
+  }
+  return false
+  
+};
