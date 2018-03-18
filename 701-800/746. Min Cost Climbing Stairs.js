a298641fdb4dd01e891a28c18e24e46c571f8454
Ali1213 <yusili2000@163.com>
2018-03-18 23:01:10 +0800

    add: resolve 745 method 2

--- 701-800/746. Min Cost Climbing Stairs.js
+++ 701-800/746. Min Cost Climbing Stairs.js
@@ -33,4 +33,16 @@ var minCostClimbingStairs = function(cost) {
     return b;
 };
 
+// beat 48.05%
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    for(let i = cost.length - 3;i >=0;i--){
+        cost[i] = cost[i] + Math.min(cost[i+1],cost[i+2]);
+    }
+    return Math.min(cost[0],cost[1]);
+};
+
 
