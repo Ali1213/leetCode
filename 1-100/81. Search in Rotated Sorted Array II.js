ce76f8f6eb4d6a651d2bba9193267edf9f216a3a
Ali1213 <yusili2000@163.com>
2018-04-14 23:07:10 +0800

    add: resolve 81

--- 1-100/81. Search in Rotated Sorted Array II.js
+++ 1-100/81. Search in Rotated Sorted Array II.js
@@ -18,14 +18,46 @@
  * @param {number} target
  * @return {boolean}
  */
-var search = function(nums, target) {
-    if(nums === null || nums.length === 0) return false;
-    if(nums[0] === target) return true;
-    for(let i = 1;i<nums.length;i++){
-        if(nums[i] === target){
+var search = function (nums, target) {
+    if (nums === null || nums.length === 0) return false;
+    if (nums[0] === target) return true;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === target) {
             return true;
-        }else if(nums[i]>target && nums[i-1]<target){
+        } else if (nums[i] > target && nums[i - 1] < target) {
             return false;
+        }
+    }
+    return false;
+};
+
+
+// beat 88.33%
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = (left + right) / 2 >> 0;
+        if (nums[mid] === target) return true;
+        if (nums[mid] < nums[right]) {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } else if (nums[mid] > nums[right]) {
+            if (target < nums[mid] && target >= nums[left]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            right = right - 1;
         }
     }
     return false;
