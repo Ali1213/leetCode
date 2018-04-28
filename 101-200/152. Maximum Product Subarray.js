4fb1f98b49e41d5f18018900386f59dc06a22f76
Ali1213 <yusili2000@163.com>
2018-04-29 01:17:01 +0800

    add: resolve 152 method 2

--- 101-200/152. Maximum Product Subarray.js
+++ 101-200/152. Maximum Product Subarray.js
@@ -12,6 +12,7 @@
 // Output: 0
 // Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 
+// beat 98.05%
 /**
  * @param {number[]} nums
  * @return {number}
@@ -33,4 +34,23 @@ var maxProduct = function(nums) {
         max = Math.max(max,v)
     }
     return max
+};
+
+// beat 98.05%
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let max = nums[0];
+    let min = nums[0];
+    let res = nums[0];
+    
+    for(let i = 1; i<nums.length;i++){
+        let temp = max;
+        max = Math.max(nums[i],max*nums[i],min*nums[i]);
+        min = Math.min(nums[i],temp*nums[i],min*nums[i]);
+        res = Math.max(max,min,res);
+    }
+    return res || 0;
 };
