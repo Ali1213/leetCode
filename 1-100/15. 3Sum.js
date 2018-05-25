7e24040d707a8a5656c286ad07689b201a589f5b
Ali1213 <yusili2000@163.com>
2018-05-25 23:27:55 +0800

    add:

--- 1-100/15. 3Sum.js
+++ 1-100/15. 3Sum.js
@@ -48,3 +48,79 @@ var threeSum = function (nums) {
 
     return results
 };
+
+// beat 53.64%
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a,b)=> a-b)
+    let results = []
+    let record = new Set();
+    for(let i = 0; i< nums.length - 1;i++){
+        if(i !== 0 && nums[i] == nums[i-1]) continue
+        let start = i+1;let end = nums.length - 1;
+        while(start < end){
+            let t = nums[i] + nums[start] + nums[end]
+            if(t>0){
+                end--;
+            }else if ( t === 0 ){
+                let r = [nums[i], nums[start], nums[end]];
+                let n = r.join('')
+                if(!record.has(n)){
+                    record.add(n)   
+                    results.push(r)
+                }
+                start++;
+                end--;
+            }else {
+                start++
+            }
+            
+        }
+        
+    }
+
+    return results
+};
+
+
+
+// beat 99.32%
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a,b)=> a-b)
+    let results = []
+    let record = new Set();
+    for(let i = 0; i< nums.length - 2;i++){
+        if(nums[i]>0) return results
+        if(i > 0 && nums[i] == nums[i-1]) continue
+        let start = i+1;let end = nums.length - 1;
+        while(start < end){
+            let t = nums[i] + nums[start] + nums[end]
+            if(t>0){
+                end--;
+            }else if ( t === 0 ){
+                results.push([nums[i], nums[start], nums[end]]);
+                start++;
+                end--;
+                while(nums[start] === nums[start-1]){
+                    start++
+                }
+                while(nums[end] === nums[end+1]){
+                    end--
+                }
+            }else {
+                start++
+            }
+            
+        }
+        
+    }
+
+    return results
+};
