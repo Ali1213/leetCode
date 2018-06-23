e5bf7449f3b50229dea25e0fd9bb58791ad02894
Ali1213 <yusili2000@163.com>
2018-06-23 19:15:56 +0800

    add:

---
+++ 501-600/560. Subarray Sum Equals K.js
@@ -0,0 +1,35 @@
+// Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+// Example 1:
+// Input:nums = [1,1,1], k = 2
+// Output: 2
+// Note:
+// The length of the array is in range [1, 20,000].
+// The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+
+
+// beat 19.51%
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let n = nums.length
+    let count = 0;
+    
+    if(n === 0) return count;
+    
+    let start = 0;
+    for(let i = 0; i<n;i++){
+        start += nums[i]
+        let total = start;
+        if(start == k) count++
+        for(let j = i+1; j<n; j++ ){
+            total = total + nums[j] - nums[j-i-1]
+            if(total == k) count++
+        }
+    }
+    return count;
+    
+};
