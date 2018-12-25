e787aca68c1b6a3f852cfdca16c7be2de518e1ca
Ali1213 <yusili2000@163.com>
2018-12-26 01:13:18 +0800

    resolve 907

--- 900-1000/907. Sum of Subarray Minimums.js
+++ 900-1000/907. Sum of Subarray Minimums.js
@@ -43,3 +43,43 @@ var sumSubarrayMins = function(A) {
     }
     return total
 };
+
+
+
+// beat 54.17%
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+
+var sumSubarrayMins = function(A) {
+    
+    let left = new Array(A.length)
+    let right = new Array(A.length)
+    left[0] = 1
+    
+    for(let i = 1; i< A.length; i++){
+        let p = i - 1;
+        while(p>=0 && A[p] >= A[i]){
+            p--
+        }
+        left[i] = i - p;
+    }
+    
+    right[A.length -1] = 1
+    for(let i = A.length -2; i>=0; i--){
+        let p = i + 1;
+        while(p< A.length && A[p] > A[i]){
+            p++
+        }
+        right[i] = p - i;
+    }
+    let total = 0;
+    
+    
+    for(let i = 0; i< A.length; i++){
+        total += left[i] * right[i] * A[i] % 1000000007
+        total = total % 1000000007
+    }
+    return total
+};
