a84c560d09f1d1e69cc75be7d9551fa2c2f6d58a
Ali1213 <yusili2000@163.com>
2019-07-24 22:32:17 +0800

    comment

--- 401-500/434. Number of Segments in a String.js
+++ 401-500/434. Number of Segments in a String.js
@@ -39,3 +39,46 @@ test.forEach(([A, r], index) => {
         throw e
     }
 })
+
+
+
+
+/**
+ * Runtime: 56 ms, faster than 56.63% of JavaScript online submissions for Number of Segments in a String.
+ * Memory Usage: 34 MB, less than 15.38% of JavaScript online submissions for Number of Segments in a String.
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function (s) {
+    let count = 0;
+    let has = false;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === " ") {
+            if (has) {
+                has = false
+                count += 1;
+            }
+        } else {
+            has = true
+        }
+    }
+    return count + Number(has)
+};
+
+
+var test = [
+    ["Hello, my name is John", 5],
+    ["    ", 0],
+    ["    a", 1],
+]
+
+
+test.forEach(([A, r], index) => {
+    try {
+        assert.deepEqual(countSegments(A), r)
+    } catch (e) {
+        console.log(`${index} occur error`)
+        console.log(e)
+        throw e
+    }
+})
