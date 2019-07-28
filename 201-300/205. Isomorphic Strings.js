97a2c4a236a8db4da3303bfec8f3f38424931044
Ali1213 <yusili2000@163.com>
2019-07-28 14:39:22 +0800

    comment

--- 201-300/205. Isomorphic Strings.js
+++ 201-300/205. Isomorphic Strings.js
@@ -35,13 +35,13 @@ var isIsomorphic = function (s, t) {
     let sm = new Map();
     let tm = new Map();
     for (let i = 0; i < s.length; i++) {
-        if(sm.has(s[i])){
-            if(sm.get(s[i]) !== t[i]) return false
+        if (sm.has(s[i])) {
+            if (sm.get(s[i]) !== t[i]) return false
         } else {
             sm.set(s[i], t[i])
         }
-        if(tm.has(t[i])){
-            if(tm.get(t[i]) !== s[i]) return false
+        if (tm.has(t[i])) {
+            if (tm.get(t[i]) !== s[i]) return false
         } else {
             tm.set(t[i], s[i])
         }
@@ -57,17 +57,103 @@ var test = [
     ["foo", "bar", false],
     ["paper", "title", true],
     ["egg", "egg", true],
-]
+];
 
 
 test.forEach(([A, B, r], index) => {
-    console.log(index)
     try {
-        assert.deepEqual(isIsomorphic(A, B), r)
+        assert.deepEqual(isIsomorphic(A, B), r);
 
     } catch (e) {
-        console.log(`===第${index + 1}===eccour error================`)
-        console.log(`===${JSON.stringify(A)}===eccour error================`)
+        console.log(`===第${index + 1}===eccour error================`);
+        console.log(`===${JSON.stringify(A)}===eccour error================`);
         throw e
     }
-})
+});
+
+
+/**
+ *
+ * 明显速度不可能变慢啊，我的天
+ * Runtime: 84 ms, faster than 18.60% of JavaScript online submissions for Isomorphic Strings.
+ * Memory Usage: 36.4 MB, less than 49.48% of JavaScript online submissions for Isomorphic Strings.
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+
+    let sm = new Map();
+    let tm = new Map();
+    for (let i = 0; i < s.length; i++) {
+        if (sm.has(s[i])) {
+            if (sm.get(s[i]) !== t[i]) return false;
+        } else if (tm.has(t[i])) {
+            if (tm.get(t[i]) !== s[i]) return false;
+        } else {
+            sm.set(s[i], t[i]);
+            tm.set(t[i], s[i]);
+        }
+
+    }
+    return true
+};
+
+
+var test = [
+    ["egg", "add", true],
+    ["foo", "bar", false],
+    ["paper", "title", true],
+    ["pacer", "title", false],
+    ["egg", "egg", true],
+];
+
+
+test.forEach(([A, B, r], index) => {
+    try {
+        assert.deepEqual(isIsomorphic(A, B), r);
+
+    } catch (e) {
+        console.log(`===第${index + 1}===eccour error================`);
+        console.log(`===${JSON.stringify(A)}===eccour error================`);
+        throw e
+    }
+});
+
+
+/**
+ *
+ * Runtime: 72 ms, faster than 46.24% of JavaScript online submissions for Isomorphic Strings.
+ * Memory Usage: 35.2 MB, less than 93.68% of JavaScript online submissions for Isomorphic Strings.
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i], i+1) !== t.indexOf(t[i], i+1)) return false
+    }
+    return true
+};
+
+
+var test = [
+    ["egg", "add", true],
+    ["foo", "bar", false],
+    ["paper", "title", true],
+    ["pacer", "title", false],
+    ["egg", "egg", true],
+];
+
+
+test.forEach(([A, B, r], index) => {
+    try {
+        assert.deepEqual(isIsomorphic(A, B), r);
+
+    } catch (e) {
+        console.log(`===第${index + 1}===eccour error================`);
+        console.log(`===${JSON.stringify(A)}===eccour error================`);
+        throw e
+    }
+});
