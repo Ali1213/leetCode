cabe788be00c375bca8c81e0f51945c4676beee7
Ali1213 <yusili2000@163.com>
2019-08-25 22:40:39 +0800

    comment

---
+++ 301-400/342. Power of Four.js
@@ -0,0 +1,44 @@
+/*
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 5
+Output: false
+Follow up: Could you solve it without loops/recursion?
+*/
+
+/**
+ * Runtime: 56 ms, faster than 98.81% of JavaScript online submissions for Power of Four.
+ * Memory Usage: 35.6 MB, less than 100.00% of JavaScript online submissions for Power of Four.
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function(num) {
+    return num > 0 && 4294967296 % 4 === 0 && Math.log2(num) % 2 === 0
+};
+
+
+
+const assert = require('assert');
+var test = [
+    [8, false],
+    [16, true],
+    [5, false],
+];
+
+
+test.forEach(([A, r], index) => {
+    try {
+
+        assert.deepEqual(isPowerOfFour(A), r)
+    } catch (e) {
+        console.log(`${index} occur error`);
+        console.log(e)
+        throw e;
+    }
+});
