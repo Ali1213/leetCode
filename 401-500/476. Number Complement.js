c5a0cffe6b0fa0526f59dca749107759bf4a9c14
Ali1213 <yusili2000@163.com>
2019-09-22 23:10:37 +0800

    comment

--- 401-500/476. Number Complement.js
+++ 401-500/476. Number Complement.js
@@ -16,9 +16,39 @@ Explanation: The binary representation of 1 is 1 (no leading zero bits), and its
 
 
 /**
+ * Runtime: 52 ms, faster than 77.22% of JavaScript online submissions for Number Complement.
+ * Memory Usage: 33.9 MB, less than 100.00% of JavaScript online submissions for Number Complement.
  * @param {number} num
  * @return {number}
  */
-var findComplement = function(num) {
-
+const findComplement = function (num) {
+    let r = 0;
+    let pos = 0;
+    while (num > 0) {
+        if (num % 2 === 0) {
+            r += (1 << pos);
+        }
+        num >>= 1;
+        pos += 1;
+    }
+    return r;
 };
+
+
+const assert = require('assert');
+
+const test = [
+    [5, 2],
+    [1, 0],
+];
+
+
+test.forEach(([A, r], index) => {
+    try {
+        assert.deepEqual(findComplement(A), r);
+    } catch (e) {
+        console.log(`${index} occur error`);
+        console.log(e);
+        throw e;
+    }
+});
