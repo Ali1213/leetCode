1f3cdd3ced4b1ae8aae4f786837e890b49262667
Ali1213 <yusili2000@163.com>
2019-12-21 21:28:32 +0800

    comment

---
+++ 601-700/700. Search in a Binary Search Tree.js
@@ -0,0 +1,62 @@
+/*
+Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+    For example,
+
+    Given the tree:
+     4
+    / \
+   2   7
+  / \
+ 1   3
+
+And the value to search: 2
+You should return this subtree:
+
+     2
+    / \
+   1   3
+In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+
+    Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null.
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * Runtime: 76 ms, faster than 82.72% of JavaScript online submissions for Search in a Binary Search Tree.
+ * Memory Usage: 41.8 MB, less than 56.25% of JavaScript online submissions for Search in a Binary Search Tree.
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+const searchBST = function (root, val) {
+    if (root === null) return [];
+
+    let res = null
+
+    const transfer = (root, val) => {
+        if (root === null) return null;
+
+        if (root.val === val) {
+            res = root;
+            throw Error('')
+        }
+        transfer(root.left, val);
+        transfer(root.right, val);
+    };
+
+    try {
+        transfer(root, val);
+    } catch (e) {
+
+    }
+
+    return res;
+};
