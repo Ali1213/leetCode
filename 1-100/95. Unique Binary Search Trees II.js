e504a7dd31b42ecfaf14afb06ab15a1d66d5fb29
Ali1213 <yusili2000@163.com>
2020-08-20 23:18:13 +0800

    comment

--- 1-100/95. Unique Binary Search Trees II.js
+++ 1-100/95. Unique Binary Search Trees II.js
@@ -23,18 +23,48 @@ The above output corresponds to the 5 unique BST's shown below:
    */
 
 
-   /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+/**
+ * Runtime: 92 ms, faster than 76.73% of JavaScript online submissions for Unique Binary Search Trees II.
+ * Memory Usage: 42.6 MB, less than 61.46% of JavaScript online submissions for Unique Binary Search Trees II.
+* Definition for a binary tree node.
+* function TreeNode(val, left, right) {
+*     this.val = (val===undefined ? 0 : val)
+*     this.left = (left===undefined ? null : left)
+*     this.right = (right===undefined ? null : right)
+* }
+*/
 /**
  * @param {number} n
  * @return {TreeNode[]}
  */
-var generateTrees = function(n) {
-    
+var generateTrees = function (n) {
+
+	const dp = new Array(n + 1)
+	dp[0] = []
+	if (n == 0) return dp[0]
+	dp[0].push(null)
+	for (let len = 1; len <= n; len++) {
+		dp[len] = []
+		for (let root = 0; root < len; root++) {
+			dp[root].forEach( leftTree => {
+				dp[len - root  - 1].forEach( rightTree => {
+					let rootTree = new TreeNode(root + 1)
+					rootTree.left = leftTree
+					rootTree.right = clone(rightTree, root + 1)
+					dp[len].push(rootTree)
+				})
+			})
+		}
+	}
+	return dp[n]
+
+	function clone(tree, offset) {
+		if (tree == null) {
+			return null
+		}
+		const node = new TreeNode(tree.val + offset)
+		node.left = clone(tree.left, offset)
+		node.right = clone(tree.right, offset)
+		return node
+	}
 };
