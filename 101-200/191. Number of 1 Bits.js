08cc69a1e12beaf7be812c13bc05c214b2a7a5b7
Ali1213 <yusili2000@163.com>
2022-11-13 16:29:54 +0800

    feat: 191

--- 101-200/191. Number of 1 Bits.js
+++ 101-200/191. Number of 1 Bits.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /*
 Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
 
@@ -16,17 +17,17 @@ Example 3:
 Input: 11111111111111111111111111111101
 Output: 31
 Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
- 
+
 
 Note:
 
 Note that in some languages such as Java, there is no unsigned integer type. In this case, the input will be given as signed integer type and should not affect your implementation, as the internal binary representation of the integer is the same whether it is signed or unsigned.
 In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3 above the input represents the signed integer -3.
- 
+
 
 Follow up:
 
-If this function is called many times, how would you optimize it? 
+If this function is called many times, how would you optimize it?
 */
 
 /**
@@ -36,15 +37,43 @@ If this function is called many times, how would you optimize it?
  * @param {number} n - a positive integer
  * @return {number}
  */
-var hammingWeight = function (n) {
-    let count = 0;
+const hammingWeight = (n) => {
+    let count = 0
     while (n > 0) {
         if (n % 2 === 1) {
-            count++;
-            n = (n - 1) / 2;
+            count++
+            n = (n - 1) / 2
         } else {
-            n = n / 2;
+            n /= 2
         }
     }
     return count
-};
+}
+
+// Runtime: 64 ms, faster than 97.52% of JavaScript online submissions for Number of 1 Bits.
+// Memory Usage: 42.1 MB, less than 83.64% of JavaScript online submissions for Number of 1 Bits.
+const hammingWeight2 = (n) => {
+    let count = 0
+    while (n !== 0) {
+        // eslint-disable-next-line no-bitwise
+        n &= (n - 1)
+        count++
+    }
+    return count
+}
+
+const { normalTest } = require('../test')
+
+
+const tests = [
+    {
+        params: [00000000000000000000000000001011],
+        result: 'bab',
+    },
+    {
+        params: ['cbbd'],
+        result: 'bb',
+    }]
+
+normalTest(tests, hammingWeight)
+normalTest(tests, hammingWeight2)
