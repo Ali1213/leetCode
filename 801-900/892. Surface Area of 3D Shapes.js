941b82118540d3a2e47eb4abeb4046bfb11a93e9
Ali1213 <yusili2000@163.com>
2022-12-04 13:30:12 +0800

    feat: 892

--- 801-900/892. Surface Area of 3D Shapes.js
+++ 801-900/892. Surface Area of 3D Shapes.js
@@ -6,7 +6,6 @@ On a N * N grid, we place some 1 * 1 * 1 cubes.
     Return the total surface area of the resulting shapes.
 
 
-
     Example 1:
 
 Input: [[2]]
@@ -39,13 +38,48 @@ Note:
  * @param {number[][]} grid
  * @return {number}
  */
-var surfaceArea = function(grid) {
+//  Runtime: 70 ms, faster than 90.67% of JavaScript online submissions for Surface Area of 3D Shapes.
+//  Memory Usage: 42.7 MB, less than 66.67% of JavaScript online submissions for Surface Area of 3D Shapes.
+const surfaceArea = (grid) => {
+    let area = 0
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] !== 0) {
+                area += grid[i][j] * 6 - (grid[i][j] - 1) * 2 - findDiff(grid, i, j)
+            }
+        }
+    }
+    return area
+}
 
-};
+const findDiff = (grid, i, j) => {
+    let diff = 0
+    if (i !== grid.length - 1) {
+        diff += Math.min(grid[i][j], grid[i + 1][j]) * 2
+    }
+    if (j !== grid[i].length - 1) {
+        diff += Math.min(grid[i][j], grid[i][j + 1]) * 2
+    }
 
-const {normalTest} = require('../test/index')
+    return diff
+}
+
+const { normalTest } = require('../test/index')
 
 const test = [
+
+    {
+        params: [[[1, 2], [3, 4]]],
+        result: 34,
+    },
+    {
+        params: [[[1, 1, 1], [1, 0, 1], [1, 1, 1]]],
+        result: 32,
+    },
+    {
+        params: [[[2, 2, 2], [2, 1, 2], [2, 2, 2]]],
+        result: 46,
+    },
 ]
 
 normalTest(test, surfaceArea)
