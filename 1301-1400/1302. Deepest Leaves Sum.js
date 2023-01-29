8e1938201b6c0853c41747ef0bb9e50d49b73e6b
Ali1213 <ali.yu@ucloud.cn>
2023-01-29 23:47:21 +0800

    feat: 1302

--- 1301-1400/1302. Deepest Leaves Sum.js
+++ 1301-1400/1302. Deepest Leaves Sum.js
@@ -29,3 +29,37 @@ var deepestLeavesSum = function(root) {
     }
     return store.reduce((prev,item) => prev + item.val, 0)
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var deepestLeavesSum = function(root) {
+    if(root == null) return 0
+    let now = [
+    root
+    ]
+    let next = []
+    let sum = 0
+    while(now.length != 0){
+        let cur = now.shift()
+        sum += cur.val
+        if(cur.left) next.push(cur.left)
+        if(cur.right) next.push(cur.right)
+        if(now.length == 0 && next.length != 0) {
+            now = next
+            next = []
+            sum = 0
+        }
+    }
+    return sum
+};
