3d45ba6f5f72a02c749707ad55785309fa51a808
Ali1213 <yusili2000@163.com>
2023-01-30 23:35:15 +0800

    temp: 797

---
+++ 701-800/797. All Paths From Source to Target.go
@@ -0,0 +1,11 @@
+package  main
+func allPathsSourceTarget(graph [][]int) [][]int {
+    ans := make([][]int, 0)
+    bfs := make([][]int, 0)
+    bfs = append(bfs, []int{0})
+
+    for ; len(bfs) > 0 ; {
+        cur := bfs[0]
+        
+    }
+}
