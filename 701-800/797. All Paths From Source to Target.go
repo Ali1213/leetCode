8592fb1d316b74ea72e4d71881d3c2b2553cc698
Ali1213 <yusili2000@163.com>
2023-01-31 23:34:38 +0800

    feat: 797

--- 701-800/797. All Paths From Source to Target.go
+++ 701-800/797. All Paths From Source to Target.go
@@ -1,4 +1,12 @@
 package  main
+// Runtime
+// 13 ms
+// Beats
+// 27.75%
+// Memory
+// 7.3 MB
+// Beats
+// 11.56%
 func allPathsSourceTarget(graph [][]int) [][]int {
     ans := make([][]int, 0)
     bfs := make([][]int, 0)
@@ -6,6 +14,16 @@ func allPathsSourceTarget(graph [][]int) [][]int {
 
     for ; len(bfs) > 0 ; {
         cur := bfs[0]
-        
+        if cur[len(cur) - 1] == len(graph) - 1 {
+            ans = append(ans, cur)
+        }
+        for _, val := range graph[cur[len(cur) - 1] ] {
+            x := make([]int,len(cur))
+            copy(x,cur)
+            x = append(x, val)
+            bfs = append(bfs, x)
+        }
+        bfs = bfs[1:]
     }
+    return ans
 }
