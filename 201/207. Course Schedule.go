40e6188af38084e71e31e1203287816610f527d1
Ali1213 <ali.yu@ucloud.cn>
2023-04-14 13:57:10 +0800

    feat: 207

--- 201/207. Course Schedule.go
+++ 201/207. Course Schedule.go
@@ -1,6 +1,5 @@
 package main
 
-import "fmt"
 
 // Runtime
 // 8 ms
@@ -9,8 +8,9 @@ import "fmt"
 // Memory
 // 6.3 MB
 // Beats
-// 76.35%
-func canFinish(numCourses int, prerequisites [][]int) bool {
+// 76.35%   
+// 这个是BFS的方法
+func canFinish1(numCourses int, prerequisites [][]int) bool {
     graph := make(map[int][]int, 0)
 	invest := make([]int, numCourses)
 	for _, arr := range prerequisites {
@@ -45,5 +45,53 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 			return false
 		}
 	}
+	return true
+}
+
+
+
+// 测试使用DFS的方法
+// Runtime
+// 17 ms
+// Beats
+// 26.44%
+// Memory
+// 6.7 MB
+// Beats
+// 29.57%
+func canFinish(numCourses int, prerequisites [][]int) bool {
+    graph := make(map[int][]int, 0)
+	for _, arr := range prerequisites {
+		if graph[arr[1]] == nil {
+			graph[arr[1]] = make([]int, 0)
+		}
+		graph[arr[1]] = append(graph[arr[1]], arr[0])
+	}
+	store := make(map[int]int, 0)
+	for i:=0; i< numCourses; i++ {
+		if !dfs(store, i, graph) {
+			return false
+		}
+	}
+	return true
+}
+
+
+func dfs(store map[int]int, i int, graph map[int][]int) bool{
+	// if store[i] == 
+	if store[i] == -1 {
+		return false
+	}
+
+	if store[i] ==  1 {
+		return true
+	}
+	store[i] = - 1
+	for _, val := range graph[i] {
+		if !dfs(store, val, graph) {
+			return false
+		}
+	}
+	store[i] = 1
 	return true
 }
