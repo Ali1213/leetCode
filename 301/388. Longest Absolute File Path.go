67ed9b1719aa65def45c7394c7584e80b4a9da5e
Ali1213 <yusili2000@163.com>
2023-05-14 23:30:43 +0800

    feat: 388

---
+++ 301/388. Longest Absolute File Path.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"strings"
+)
+// Runtime
+// 0 ms
+// Beats
+// 100%
+// Memory
+// 2 MB
+// Beats
+// 100%
+
+func lengthLongestPath(input string) int {
+	lines := strings.Split(input, "\n")
+	maxLen := 0 
+	pathLen := make(map[int]int, 0)
+	for _, line := range lines {
+		name := strings.TrimLeft(line, "\t")
+		depth := len(line) - len(name)
+		
+		if strings.Contains(name,".") {
+			maxLen = max(maxLen, pathLen[depth] + len(name) + 1)
+		} else {
+			pathLen[depth + 1] = pathLen[depth] + len(name) + 1
+		}
+	}
+	return maxLen
+}
+
+
+func max(a, b int) int{
+	if a > b {
+		return a
+	}
+	return b
+}
